5d6613cf4aa89f95efbd67b62a262e891f7589d1
Raj Posture <rajposture16@gmail.com>
2026-02-17 21:55:52 +0530

    chnages

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://class-mark.vercel.app"
+    ],
     credentials: true,
   })
 );
