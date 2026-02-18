946ac46c807c30d711d507ac2b82c0a1fb5df899
Raj Posture <rajposture16@gmail.com>
2026-02-18 13:49:41 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import "./index.css";   // ✅ THIS IS REQUIRED
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>   {/* ✅ THIS IS REQUIRED */}
+    <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
