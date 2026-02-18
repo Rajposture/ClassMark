baf7da381586d017fa1f7a09c9e707ceb361e40b
Raj Posture <rajposture16@gmail.com>
2026-02-18 13:34:57 +0530

    fine all done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")).render(
-<BrowserRouter>
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-</BrowserRouter>
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>   {/* ✅ THIS IS REQUIRED */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
 );
