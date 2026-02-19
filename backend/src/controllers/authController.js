b7156730903f7b686bc5247309aa5c5c0c0662ee
Raj Posture <rajposture16@gmail.com>
2026-02-19 12:50:34 +0530

    soo

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -17,12 +17,15 @@ dotenv.config();
 const pendingUsers = new Map();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 const sendMail = async ({ to, subject, html }) => {
   try {
