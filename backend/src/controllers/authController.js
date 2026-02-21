b721f5861f4ef3fd63add840af6e92ce4ec1d055
Raj Posture <rajposture16@gmail.com>
2026-02-21 14:12:53 +0530

    transporter

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -10,9 +10,7 @@ import otpEmailTemplate from "./otpEmailTemplate.js";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
