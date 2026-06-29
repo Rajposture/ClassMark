196db784c09bba9b1c523c9d651ecebb4552efe2
Raj Posture <rajposture16@gmail.com>
2026-06-29 21:52:27 +0530

    rate limit fixed

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,11 +1,12 @@
-import rateLimit from "express-rate-limit"
+import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000, // 1000 requests per IP every 15 minutes
   message: {
     message: "Too many requests. Please try again later."
   },
   standardHeaders: true,
-  legacyHeaders: false
-})
+  legacyHeaders: false,
+  skipSuccessfulRequests: false
+});
